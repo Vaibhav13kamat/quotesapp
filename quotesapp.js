a46c51815efab0469cb5bd8f7c3eeb96c674729a
sidesai2001 <sidesai2001@gmail.com>
2022-09-01 23:56:37 +0530

    task 3 completed

--- quotesapp.js
+++ quotesapp.js
@@ -1,5 +1,3 @@
-
-
 function quotes_button() {
     fetch("https://quote-api-app.herokuapp.com/quote").then((data) => {
         return data.json(); //converted to object
@@ -19,6 +17,7 @@ function quotes_button() {
         document.getElementById("names").innerHTML = list1;
       });
   }
+
   function author_button() {
     fetch("https://quote-api-app.herokuapp.com/author").then((data2) => {
         return data2.json(); //converted to object
@@ -34,3 +33,33 @@ function quotes_button() {
         document.getElementById("name").innerHTML = list2;
       });
   }
+
+function search_button() {
+
+  let users = []
+  const url = "https://quote-api-app.herokuapp.com/quote/search?author="
+  const value = document.getElementById('input_text').value
+  const api = url + value  
+
+  fetch(api).then((data3) => {
+        return data3.json(); //converted to object
+      })
+      .then((info3) => {
+        users = info3.map(user => {
+          
+          console.log(info3[0]);
+          let list2 = "";
+          info3.map((values) => {
+            list2 += `   
+                <h5>"${values.quote}"</h5>
+                <p><b >&nbsp~${values.author}</b></p>
+                <li>Likes:&nbsp${values.likes}</li>
+                <li>Dislikes:&nbsp${values.dislikes}</li>
+                  <br>  <br>     
+            `;
+          });
+          document.getElementById("names").innerHTML = list2;
+        });
+      });
+      
+}
