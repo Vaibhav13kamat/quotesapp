8cefea1f8ae8013f55df1ebb477304a7f89b6ee9
sidesai2001 <sidesai2001@gmail.com>
2022-09-02 11:26:46 +0530

    onclick added to author list

--- quotesapp.js
+++ quotesapp.js
@@ -30,36 +30,42 @@ function quotes_button() {
               <li>${value}</li>
            `;
         });
+        document.getElementById("name").addEventListener("click", myFunction);
         document.getElementById("name").innerHTML = list2;
+        
       });
   }
 
-function search_button() {
+  function search_button() {
 
-  let users = []
-  const url = "https://quote-api-app.herokuapp.com/quote/search?author="
-  const value = document.getElementById('input_text').value
-  const api = url + value  
+    let users = []
+    const url = "https://quote-api-app.herokuapp.com/quote/search?author="
+    const value = document.getElementById('input_text').value
+    const api = url + value  
 
-  fetch(api).then((data3) => {
-        return data3.json(); //converted to object
-      })
-      .then((info3) => {
-        users = info3.map(user => {
-          
-          console.log(info3[0]);
-          let list2 = "";
-          info3.map((values) => {
-            list2 += `   
-                <h5>"${values.quote}"</h5>
-                <p><b >&nbsp~${values.author}</b></p>
-                <li>Likes:&nbsp${values.likes}</li>
-                <li>Dislikes:&nbsp${values.dislikes}</li>
-                  <br>  <br>     
-            `;
+    fetch(api).then((data3) => {
+          return data3.json(); //converted to object
+        })
+        .then((info3) => {
+          users = info3.map(user => {
+            
+            console.log(info3[0]);
+            let list2 = "";
+            info3.map((values) => {
+              list2 += `   
+                  <h5>"${values.quote}"</h5>
+                  <p><b >&nbsp~${values.author}</b></p>
+                  <li>Likes:&nbsp${values.likes}</li>
+                  <li>Dislikes:&nbsp${values.dislikes}</li>
+                    <br>  <br>     
+              `;
+            });
+            document.getElementById("names").innerHTML = list2;
           });
-          document.getElementById("names").innerHTML = list2;
         });
-      });
+    }
+
+    function myFunction() {
+      document.getElementById("names").innerHTML = "YOU CLICKED ME!";
       
-}
+    }
